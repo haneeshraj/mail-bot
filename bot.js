e51172fc681849188ad3d2e96969f13728e689cb
Haneesh Raj <haneeshraj.banisetti@gmail.com>
2022-07-25 19:06:41 +0530

    Add Permissions

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const {
   Client,
   GatewayIntentBits,
   EmbedBuilder,
-  Embed,
+  PermissionFlagsBits,
 } = require("discord.js");
 require("dotenv").config();
 
@@ -32,95 +32,102 @@ client.on("messageCreate", async (message) => {
       .split(/\s+/);
 
     if (command === "announce") {
-      if (args[0] === "channel") {
-        announceChannel = args[1].slice(2, args[1].length - 1);
+      if (
+        message.member.permissions.has(PermissionFlagsBits.ManageChannels) ||
+        message.member.permissions.has(PermissionFlagsBits.Administrator)
+      ) {
+        if (args[0] === "channel") {
+          announceChannel = args[1].slice(2, args[1].length - 1);
 
-        const setEmbed = new EmbedBuilder()
-          .setColor("Green")
-          .setThumbnail(message.guild.iconURL())
-          .setTitle("Success!")
-          .setDescription(
-            `Announcement channel has been set to <#${announceChannel}>`
-          );
-        return await message.channel.send({ embeds: [setEmbed] });
-      }
-      if (args[0] === "role") {
-        announceRole = args[1].slice(3, args[1].length - 1);
-        const setEmbed = new EmbedBuilder()
-          .setColor("Green")
-          .setThumbnail(message.guild.iconURL())
-          .setTitle("Success!")
-          .setDescription(
-            `Announcement role has been set to <@&${announceRole}>`
-          );
-        return await message.channel.send({ embeds: [setEmbed] });
-      }
-      if (args[0] === "broadcast") {
-        if (!announceChannel || !announceRole) {
-          const errorEmbed = new EmbedBuilder()
-            .setColor("Red")
+          const setEmbed = new EmbedBuilder()
+            .setColor("Green")
             .setThumbnail(message.guild.iconURL())
-            .setTitle("Error!")
+            .setTitle("Success!")
             .setDescription(
-              !announceChannel && !announceRole
-                ? "Please assign the announce role and channel by using the \n`>announce channel #channel-name` and \n`>announce role @role`"
-                : !announceRole
-                ? "Please assign the announce role by using \n`>announce channel @role`\n**Note** : Make sure thee role is mentionable"
-                : !announceChannel
-                ? "Please assign the announce channel by using \n`>announce channel #channel-name`"
-                : ""
+              `Announcement channel has been set to <#${announceChannel}>`
             );
-          return await message.channel.send({ embeds: [errorEmbed] });
+          return await message.channel.send({ embeds: [setEmbed] });
         }
-        const [_, ...announcementContent] = args;
-        if (
-          !announcementContent.join(" ").includes("title:") ||
-          !announcementContent.join(" ").includes("desc:")
-        ) {
-          const syntaxErrorEmbed = new EmbedBuilder()
-            .setColor("Red")
+        if (args[0] === "role") {
+          announceRole = args[1].slice(3, args[1].length - 1);
+          const setEmbed = new EmbedBuilder()
+            .setColor("Green")
             .setThumbnail(message.guild.iconURL())
-            .setTitle("Error!")
-            .setDescription("You havent given the right syntax!")
-            .setFields({
-              name: "Right way to use the coomand",
-              value:
-                "```>announce broadcast title: Title Here desc: Description here ```",
-            });
-          return message.channel.send({ embeds: [syntaxErrorEmbed] });
-        }
-        const [BCtitle, BCDesc] = announcementContent
-          .join(" ")
-          .split("title:")[1]
-          .split("desc:");
-
-        const membersRole = message.guild.roles.cache
-          .get(announceRole)
-          .members.map((m) => m.user.id);
-
-        const announceEmbed = new EmbedBuilder()
-          .setAuthor({
-            name: message.guild.name,
-          })
-          .setThumbnail(message.guild.iconURL())
-          .setTitle(BCtitle)
-          .setDescription(BCDesc);
-
-        membersRole.forEach(async (subscribedUser) => {
-          try {
-            return await message.guild.members.cache
-              .get(subscribedUser)
-              .send({ embeds: [announceEmbed] });
-          } catch (err) {
-            console.log(
-              "could not the announcements to " +
-                client.users.cache.get(subscribedUser).tag
+            .setTitle("Success!")
+            .setDescription(
+              `Announcement role has been set to <@&${announceRole}>`
             );
+          return await message.channel.send({ embeds: [setEmbed] });
+        }
+        if (args[0] === "broadcast") {
+          if (!announceChannel || !announceRole) {
+            const errorEmbed = new EmbedBuilder()
+              .setColor("Red")
+              .setThumbnail(message.guild.iconURL())
+              .setTitle("Error!")
+              .setDescription(
+                !announceChannel && !announceRole
+                  ? "Please assign the announce role and channel by using the \n`>announce channel #channel-name` and \n`>announce role @role`"
+                  : !announceRole
+                  ? "Please assign the announce role by using \n`>announce channel @role`\n**Note** : Make sure thee role is mentionable"
+                  : !announceChannel
+                  ? "Please assign the announce channel by using \n`>announce channel #channel-name`"
+                  : ""
+              );
+            return await message.channel.send({ embeds: [errorEmbed] });
           }
-        });
-        const sendAnnouncement =
-          message.client.channels.cache.get(announceChannel);
-        sendAnnouncement.send({ embeds: [announceEmbed] });
+          const [_, ...announcementContent] = args;
+          if (
+            !announcementContent.join(" ").includes("title:") ||
+            !announcementContent.join(" ").includes("desc:")
+          ) {
+            const syntaxErrorEmbed = new EmbedBuilder()
+              .setColor("Red")
+              .setThumbnail(message.guild.iconURL())
+              .setTitle("Error!")
+              .setDescription("You havent given the right syntax!")
+              .setFields({
+                name: "Right way to use the coomand",
+                value:
+                  "```>announce broadcast title: Title Here desc: Description here ```",
+              });
+            return message.channel.send({ embeds: [syntaxErrorEmbed] });
+          }
+          const [BCtitle, BCDesc] = announcementContent
+            .join(" ")
+            .split("title:")[1]
+            .split("desc:");
+
+          const membersRole = message.guild.roles.cache
+            .get(announceRole)
+            .members.map((m) => m.user.id);
+
+          const announceEmbed = new EmbedBuilder()
+            .setAuthor({
+              name: message.guild.name,
+            })
+            .setThumbnail(message.guild.iconURL())
+            .setTitle(BCtitle)
+            .setDescription(BCDesc);
+
+          membersRole.forEach(async (subscribedUser) => {
+            try {
+              return await message.guild.members.cache
+                .get(subscribedUser)
+                .send({ embeds: [announceEmbed] });
+            } catch (err) {
+              console.log(
+                "could not the announcements to " +
+                  client.users.cache.get(subscribedUser).tag
+              );
+            }
+          });
+          const sendAnnouncement =
+            message.client.channels.cache.get(announceChannel);
+          sendAnnouncement.send({ embeds: [announceEmbed] });
+        }
+      } else {
+        console.log("man wants to be a haccer");
       }
     }
   }
